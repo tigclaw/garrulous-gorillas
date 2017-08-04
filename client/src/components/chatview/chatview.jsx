b846f74fe156afb54716705d44e871f32e1727c3
Angela Lin <tigclaw@gmail.com>
2017-08-04 14:57:58 -0400

    Try edit chat box

--- client/src/components/chatview/chatview.jsx
+++ client/src/components/chatview/chatview.jsx
@@ -28,55 +28,40 @@ export default class Chatview extends React.Component {
     socket.on('chat', (data) => {
       const output = ReactDOM.findDOMNode(this.refs.outputSpectator);
       console.log(output);
-      output.innerHTML += `<p><strong>${data.username}:</strong>${data.message}</p>`;
+      output.innerHTML += `<p><strong>${data.username}:</strong>  ${data.message}</p>`;
       output.lastChild.scrollIntoView();
     });
   }
 
  render() {
   return (
-      <div className="container">
-        <div className="row">
-            <div className="col-md-5">
-                <div className="panel panel-primary">
-                    <div className="panel-heading" id="accordion">
-                        <span className="glyphicon glyphicon-comment"></span> Spectator
-                          <div className="btn-group pull-right">
-                            <a type="button" className="btn btn-default btn-xs" data-toggle="collapse" href="#collapse1">
-                                <span className="glyphicon glyphicon-chevron-down"></span>
-                            </a>
-                          </div>
-                    </div>
-                    <div className="panel-collapse collapse in" id="collapse1">
-                      <div className="panel-body">
-                        <div id="chat-window-output" ref="outputSpectator"></div>
-                      </div>
-                      <ul className="pages">
-                        <li className="chat page">
-                          <div className="chatArea">
-                            <ul className="messages"></ul>
-                          </div>
-                        </li>
-                        <li className="login page">
-                          <div className="form">
-                            <h3 className="title">What's your nickname?</h3>
-                            <input className="usernameInput" ref="username" type="text" maxLength="14" />
-                          </div>
-                        </li>
-                      </ul>
-                      <div className="panel-footer">
-                        <div className="input-group">
-                            <input id="btn-input" type="text" className="inputMessage form-control input-sm" ref="message" placeholder="Type your message here..." />
-                            <span className="input-group-btn">
-                                <button className="btn btn-warning btn-sm" id="btn-chat" onClick={(event) => this.handleSubmit(event)}>Send</button>
-                            </span>
-                        </div>
-                      </div> 
-                    </div> 
-                </div>
-            </div>      
+      <div className="chat-container">
+        <span className="user-role">Spectator</span><span className="glyphicon glyphicon-comment"></span>
+        <div className="panel-body">
+          <div id="chat-window-output" ref="outputSpectator"></div>
         </div>
-      </div>     
-      )
-   }
+        
+        <ul className="pages">
+          <div className="form">
+            <span className="title">Nickname?</span>
+            <input className="usernameInput" ref="username" type="text" maxLength="14" />
+          </div>
+          <li className="chat page">
+            <div className="chatArea">
+              <ul className="messages"></ul>
+            </div>
+          </li>
+        </ul>
+
+        <div className="input-group">
+          <input id="btn-input" type="text" className="inputMessage form-control input-sm" ref="message" placeholder="Type your message here..." />
+          <span className="input-group-btn">
+            <button className="btn btn-warning btn-sm" id="btn-chat" onClick={(event) => this.handleSubmit(event)}>Send</button>
+          </span>
+        </div>
+
+      </div>
+       
+    )
+  }
 }
